b62c7c102764d22509e7d8a047e5b3a260e40f02
Aaron <48164620+aaronsang@users.noreply.github.com>
2023-05-15 15:10:29 -0400

    initial commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body('email') email: string, @Body('password') password: string) {
+    console.log(email);
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+}
